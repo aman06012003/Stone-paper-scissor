99385991397afcb585fb73f13cc6db949cf0a69d
Aman Kapoor <akalasklo@gmail.com>
2021-06-11 01:34:28 +0530

    Added the program

--- SPS.py
+++ SPS.py
@@ -1,1 +1,23 @@
 
+import random
+def plays():
+    user = input("What's your choice ? ('r' for rock,'s' for scissors, 'p' for paper)  ")
+    computer = random.choice(['r','s','p'])
+
+    if user == computer:
+        print('Its a tie....')
+
+    # r>s, s>p, p>r
+    if is_win(computer, user):
+        return 'Congrats you won!!'
+
+    return 'Sorry you lost'
+
+def is_win(opponent, player):
+    # return true if oppennent wins
+    if (opponent == 'r' and player == 's') or (opponent == 's' and player == 'p') or (opponent == 'p' and player == 'r'):
+        return True
+
+
+
+print(plays())
